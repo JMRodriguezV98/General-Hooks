9423a92e72554f63b571e173dbd30b7087162e32
JMRodriguezV98 <mriciosama@gmail.com>
2024-01-18 18:26:02 -0500

    seccion completa de profundizacion en hooks, seccion 11

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { MainApp } from './09-useContext/MainApp'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-// import { CallbackHook } from './06-memos/CallbackHook'
-// import { Padre } from './07-tarea-memo/Padre'
-// import './08-useReducer/intro-reducer'
-// import { MemoHook } from './06-memos/MemoHook'
-// import FormWithCustomHook from './02-useEffect/FormWithCustomHook'
-// import MultipleCustomHooks from './03-examples/MultipleCustomHooks'
-// import HooksApp from './HooksApp.jsx'
-// import CounterApp from './01-useState/CounterApp.jsx'
-// import CounterWithCustomHook from './01-useState/CounterWithCustomHook.jsx'
-// import SimpleForm from './02-useEffect/SimpleForm.jsx'
-// import { FocusScreen } from './04-useRef/FocusScreen'
-// import Layaout from './05-useLayoutEffect/Layout'
-// import { Memorize } from './06-memos/Memorize'
-import { TodoApp } from './08-useReducer/TodoApp'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    // {/* <HooksApp /> */}
-    // {/* <CounterApp /> */}
-    // {/* <CounterWithCustomHook /> */}
-    // <SimpleForm />
-    // <FormWithCustomHook />
-    // <MultipleCustomHooks />
-    // <FocusScreen />
-    // <Layaout />
-    // <Memorize />
-    // <MemoHook />
-    // <CallbackHook />
-    // <Padre />
-    <TodoApp />
-  // </React.StrictMode>,
+    <BrowserRouter>
+      {/* <React.StrictMode> */}
+      <MainApp />
+      {/* </React.StrictMode>, */}
+    </BrowserRouter>
 )
